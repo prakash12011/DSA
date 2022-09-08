8fcb01f8633e9404f11172f517781c0315e6c598
Prakash <prakashverma12011@gmail.com>
2022-09-08 22:02:05 +0530

    07 patterns created in c++ with while loop

---
+++ C++/Patterns/01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, j, i = 1;
+  cout << "Enter n" << endl;
+  cin >> n;
+
+  while (i <= n) {
+    j = 1;
+    while ( j <= n ) {
+      cout << "*";
+      j++;
+    }
+    cout << endl;
+    i++;
+  }  
+}
---
+++ C++/Patterns/08.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, j, i = 1;
+  cout << "Enter n" << endl;
+  cin >> n;
+  
+  while ( i <= n) {
+    j = 1;
+    while (j <= i) {
+      cout << i + j - 1;
+      j++;
+    }
+    cout << endl;
+    i++;
+  }  
+}
